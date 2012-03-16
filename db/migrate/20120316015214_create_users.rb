6d189afc29f736d4f66e0be0d1ee5dff8f8e8177
Jorge <just.koki@gmail.com>
2012-03-15 20:54:31 -0500

    solucion pregunta 3

---
+++ db/migrate/20120316015214_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.date :date_of_birth
+
+      t.timestamps
+    end
+  end
+end
