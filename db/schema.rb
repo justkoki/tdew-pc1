15a3426f893be670b7badd594c79e53a1e9f6a72
Jorge <just.koki@gmail.com>
2012-03-15 20:56:16 -0500

    solucion pregunta 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120316015214) do
+ActiveRecord::Schema.define(:version => 20120316015503) do
+
+  create_table "roles", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
